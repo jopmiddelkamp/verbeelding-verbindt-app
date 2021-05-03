221e834995d51fb8b1363d9a07b8c47b297253f4
Jop Middelkamp <middelkamp@live.nl>
2021-05-03 18:43:30 +0800

    Add global clean script

---
+++ flutter_clean.sh
@@ -0,0 +1,8 @@
+cd bootstrap
+flutter clean && flutter pub get
+cd ../core
+flutter clean && flutter pub get
+cd ../data
+flutter clean && flutter pub get
+cd ../presentation
+flutter clean && flutter pub get
