df9510cb81709056e6699542bc935168b4f9baa2
Jop Middelkamp <middelkamp@live.nl>
2021-06-05 16:35:28 +0800

    Re-enabled the geo locator

--- presentation/lib/shared/services/location/geo_locator/geo_locator_location_service_impl.dart
+++ presentation/lib/shared/services/location/geo_locator/geo_locator_location_service_impl.dart
@@ -3,28 +3,30 @@ import 'package:verbeelding_verbindt_core/entities/common/location.dart';
 import 'package:verbeelding_verbindt_core/services/location_service.dart';
 import 'package:verbeelding_verbindt_core/services/service_base.dart';
 
+import 'extensions/position_extensions.dart';
+
 class GlLocationServiceImpl extends ServiceBase implements LocationService {
   @override
   Future<LocationEntity> getCurrentLocation({
     LocationAccuracy locationAccuracy = LocationAccuracy.best,
   }) async {
     // // TODO: remove when Geo Locattor is working again
-    return LocationEntity(latitude: 52.393309, longitude: 6.1430403);
-    // final position = await gl.Geolocator.getCurrentPosition(
-    //   desiredAccuracy: accuracyMap[locationAccuracy]!,
-    // );
-    // return position.toLocationModel();
+    // return LocationEntity(latitude: 52.393309, longitude: 6.1430403);
+    final position = await gl.Geolocator.getCurrentPosition(
+      desiredAccuracy: accuracyMap[locationAccuracy]!,
+    );
+    return position.toLocationModel();
   }
 
   @override
   Future<LocationEntity?> getLastKnownLocation() async {
     // // TODO: remove when Geo Locattor is working again
-    return LocationEntity(latitude: 52.393309, longitude: 6.1430403);
-    // final lastPostion = await gl.Geolocator.getLastKnownPosition();
-    // if (lastPostion == null) {
-    //   return await getCurrentLocation();
-    // }
-    // return lastPostion.toLocationModel();
+    // return LocationEntity(latitude: 52.393309, longitude: 6.1430403);
+    final lastPostion = await gl.Geolocator.getLastKnownPosition();
+    if (lastPostion == null) {
+      return await getCurrentLocation();
+    }
+    return lastPostion.toLocationModel();
   }
 
   Map<LocationAccuracy, gl.LocationAccuracy> accuracyMap = {
