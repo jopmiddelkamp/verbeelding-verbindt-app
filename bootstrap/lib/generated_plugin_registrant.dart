93c05839c803ba4dbe089d493a10427191a0f2c2
Jop Middelkamp <middelkamp@live.nl>
2021-08-13 15:51:02 +0200

    Versions upgrades

--- bootstrap/lib/generated_plugin_registrant.dart
+++ bootstrap/lib/generated_plugin_registrant.dart
@@ -6,7 +6,6 @@
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:cloud_functions_web/cloud_functions_web.dart';
-import 'package:entry/entry_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
@@ -20,7 +19,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseFunctionsWeb.registerWith(registrar);
-  EntryWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
