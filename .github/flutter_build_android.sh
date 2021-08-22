6ba727a855c5e9e748d31541ccc734f79a05aa8b
Jop Middelkamp <middelkamp@live.nl>
2021-08-22 17:14:02 +0200

    Updated GithubActions

--- .github/flutter_build_android.sh
+++ .github/flutter_build_android.sh
@@ -1,3 +1,13 @@
+while getopts t:f: flag
+do
+    case "${flag}" in
+        t) target=${OPTARG};;
+        f) flavor=${OPTARG};;
+    esac
+done
+echo "target: $target"
+echo "flavor: $flavor"
+exit 0;
 cd bootstrap
-flutter build apk --release
+flutter build apk --target=$target --flavor $flavor --release
 cd ..
--- .github/flutter_build_ios.sh
+++ .github/flutter_build_ios.sh
@@ -1,3 +1,10 @@
+while getopts t:f: flag
+do
+    case "${flag}" in
+        t) target=${OPTARG};;
+        f) flavor=${OPTARG};;
+    esac
+done
 cd bootstrap
-flutter build ios --release --no-codesign
+flutter build ios --release --no-codesign --target=$target --flavor $flavor
 cd ..
