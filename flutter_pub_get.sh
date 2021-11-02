35f41d257ae61242f2cc0375beeedd84213ffabb
Jop Middelkamp <middelkamp@live.nl>
2021-11-02 22:00:36 +0100

    Another script update

--- flutter_pub_get.sh
+++ flutter_pub_get.sh
@@ -1,13 +1,19 @@
 cd verbeelding_verbindt_core
+echo "cd verbeelding_verbindt_core"
 fvm flutter pub get
 cd ..
+echo "cd .."
 
 cd verbeelding_verbindt_data
+echo "cd verbeelding_verbindt_data"
 fvm flutter pub get
 cd ..
+echo "cd .."
 
 cd verbeelding_verbindt_ui
+echo "cd verbeelding_verbindt_ui"
 fvm flutter pub get
 cd ..
+echo "cd .."
 
 fvm flutter pub get
