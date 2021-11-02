40a03713868b3a91c7b48594ed444f04cb50bb8b
Jop Middelkamp <middelkamp@live.nl>
2021-11-02 21:48:18 +0100

    Recovered some build scripts

--- verbeelding_verbindt_ui/flutter_build_runner.sh
+++ verbeelding_verbindt_ui/flutter_build_runner.sh
@@ -1,2 +1,6 @@
-fvm flutter pub run build_runner build --delete-conflicting-outputs
-fvm flutter pub run build_runner watch --delete-conflicting-outputs
+if [ "$1" == "build" ]
+then
+    fvm flutter pub run build_runner build --delete-conflicting-outputs
+else
+  fvm flutter pub run build_runner watch --delete-conflicting-outputs
+fi
